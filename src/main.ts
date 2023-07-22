84c3f6e12247e1e29dc3ff3f6cd8636cf6453fe8
mansurakhmad <mansurakhmad.051294@gmail.com>
2023-07-22 10:58:42 +0300

    setup base folders structure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './app/App.vue';
+import router from './app/router';
+import store from './app/store';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
